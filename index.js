6a1f521e478b0cdc67d5205633aa26a2352207f4
crazyrems <tauziac0remy+fork@gmail.com>
2021-02-18 00:32:52 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const https = require("https-localhost")()
+const path = require("path")
+// https is an express https, do what you usually do with express
+
+https.get('*', (req, res) => {
+	// console.log(req);
+	res.sendFile(path.join(__dirname + req.url))
+})
+
+https.listen()
---
+++ readme.md
@@ -0,0 +1,5 @@
+# A-Frame portfolio
+
+## Local server
+
+To test the project, type `node index` in the terminal.
